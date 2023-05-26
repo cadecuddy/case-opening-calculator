bd2d2bee65a98774474149361414450ffc27b981
cade <cadecuddy@gmail.com>
2023-05-26 11:51:06 -0500

    Alter FAQ color scheme

--- site/src/pages/faq.tsx
+++ site/src/pages/faq.tsx
@@ -5,15 +5,15 @@ const faq: React.FC = () => {
   return (
     <Layout>
       <div className="flex flex-col items-center justify-center space-y-8">
-        <h1 className="mb-8 text-center text-4xl font-bold text-slate-300">
+        <h1 className="mb-8 text-center text-4xl font-bold text-neutral-200">
           Frequently Asked Questions
         </h1>
         <div className="max-w-2xl space-y-8">
           <div>
-            <h2 className="text-2xl font-semibold text-slate-400">
+            <h2 className="text-2xl font-semibold text-neutral-200">
               How does the cost calculator work?
             </h2>
-            <p className="leading-7 text-neutral-200">
+            <p className="leading-7 text-slate-400">
               Total Cost ={" "}
               <span className="text-green-500">
                 (case price + key price) • case quantity
@@ -21,45 +21,45 @@ const faq: React.FC = () => {
             </p>
           </div>
           <div>
-            <h2 className="text-2xl font-semibold text-slate-400">
+            <h2 className="text-2xl font-semibold text-neutral-200">
               How do I use the cost calculator?
             </h2>
-            <p className="leading-7 text-neutral-50">
+            <p className="leading-7 text-slate-400">
               Select cases from the main page and enter the amount of each case
               you want to unbox. The total cost will be calculated and
               displayed.
             </p>
           </div>
           <div>
-            <h2 className="text-2xl font-semibold text-slate-400">
+            <h2 className="text-2xl font-semibold text-neutral-200">
               Where are the prices sourced from?
             </h2>
-            <p className="leading-7 text-neutral-50">
+            <p className="leading-7 text-slate-400">
               They&apos;re fetched directly from the Steam marketplace and are
               updated every 15 minutes or so
             </p>
           </div>
           <div>
-            <h2 className="text-2xl font-semibold text-slate-400">
+            <h2 className="text-2xl font-semibold text-neutral-200">
               How often are currency exchange rates updated?
             </h2>
-            <p className="leading-7 text-neutral-50">Every 24 hours.</p>
+            <p className="leading-7 text-slate-400">Every 24 hours.</p>
           </div>
           <div>
-            <h2 className="text-2xl font-semibold text-slate-400">
+            <h2 className="text-2xl font-semibold text-neutral-200">
               Do you guarantee any profit from unboxing?
             </h2>
-            <p className="leading-7 text-neutral-50">
+            <p className="leading-7 text-slate-400">
               No, unboxing is a game of chance with a risk of loss. This tool
               estimates unboxing costs, not financial outcomes.
             </p>
           </div>
           <div>
-            <h2 className="text-2xl font-semibold text-slate-400">
+            <h2 className="text-2xl font-semibold text-neutral-200">
               How do <span className="text-green-500">donations </span>
               work?
             </h2>
-            <p className="leading-7 text-neutral-50">
+            <p className="leading-7 text-slate-400">
               If you feel this tool saved you some time you can donate skins via
               the &quot;Donate&quot; button. All skins are appreciated, but
               certainly not necessary.
